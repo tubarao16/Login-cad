dfea5b30a54f74705495f45f882bb136bf9fbf11
Nicolas Modesto Jose <jose.nicolas@alunoescolassociais.org.br>
2024-07-04 11:50:45 -0300

    Comecei javascript de login e cadastro e adicionei neles a função de alerte caso os campos obrigatorios não fossem preenchidos corretamente, e coloquei a função onclick

---
+++ controller.js
@@ -0,0 +1,20 @@
+function acessar(){
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+  
+       if(!loginEmail || !loginSenha){
+          alert('favor preencher todos os campos');
+  
+       }else{
+        // else{ alert('campos preenchisdos com sucesso'); }
+        window.location.hrefc = 'cadastro.html'
+       }
+ }
+  
+ // FUNÇÃO PARA ARMAZENAMENTO DE NOMES EM ARRAY
+ //------------------------------------------------------------------------------------------------------------------------
+ var dadosLista = [];
+ function salvarUser(){
+    
+  
+ }
